21851514de386a3788d6d1c18d24af0af84ed43e
Noel <Noel>
2026-01-21 09:42:20 +0100

    example vissza adva

---
+++ config/config-example.php
@@ -0,0 +1,9 @@
+
+<?php
+// Ezt file-t töltsd ki a megfelelő adatokkal majd nevezd át config.php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'adatbazis',
+    'db_user' => 'felhasznalo',
+    'db_pass' => 'TITKOS_JELSZO'
+];
